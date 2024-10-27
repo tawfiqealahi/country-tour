178b4f478eab34d9ac82448cb02c35c1fa643975
tawfiq <tawfiqealahi99@gmail.com>
2024-10-27 18:44:55 +0600

    country bg add

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -7,17 +7,30 @@ import Country from "../Country/Country";
 
 const Countries = () => {
     const [countries,setCountries]=useState([]);
+    const [visitedCountries,setVisitedCountries]=useState([]);
+
+
 useEffect(()=>{
     fetch('https://restcountries.com/v3.1/all')
     .then(res=>res.json())
     .then(data=>setCountries(data));
 },[])
+
+const HandleVisitedCountry =(country)=>{
+
+}
     return (
         <div>
         <h3>Total Country: {countries.length}</h3>
+        <div>
+            <h5>Visited Country List : {} </h5>
+            <ul>
+
+            </ul>
+        </div>
      <div className='country-container'
 > {  
-        countries.map(country=><Country  key={country.cca3}
+        countries.map(country=><Country HandleVisitedCountry={HandleVisitedCountry}  key={country.cca3}
         country={country} ></Country>)
        }
        </div>
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import  './Country.css';
 
-const Country = ({country}) => {
+const Country = ({country,HandleVisitedCountry}) => {
     const {name,cca3,flags,area,capital}=country;
     const[visited,setVisited]=useState(false);
     const handleVisited=()=>{
@@ -9,14 +9,14 @@ const Country = ({country}) => {
     }
 
     return (
-        <div className='country'>
+        <div  style={{color: visited?'#dacece':'#5e8584'}} className={`country ${visited?'visited-country':'non-visited-country' } `}>
            <div className='image'> <img src={flags.png} alt="flag"  /></div>
-            <h3>Country Name:{name?.common} </h3> 
+            <h3 >Country Name:{name?.common} </h3> 
             <p>Country Code:  {cca3}</p> 
             <p>Area:{area}</p>
             <p>Capital: {capital}</p>
            <div className='button'>
-            <button onClick={handleVisited} >{visited?'Visited':'Make a plan' }</button>
+            <button onClick={HandleVisitedCountry} onClick={handleVisited} >{visited?'Visited':'Make a plan' }</button>
             {visited ?'I have visited this country.':'I want to visit this country'}
             </div>
             
